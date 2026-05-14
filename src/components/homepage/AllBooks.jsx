f0bdefd98312ec759ceeb95c493d373a04396b75
ashik-billah <ashik.210101@s.pust.ac.bd>
2026-05-14 08:43:16 +0600

    add book container

--- src/components/homepage/AllBooks.jsx
+++ src/components/homepage/AllBooks.jsx
@@ -11,7 +11,7 @@ const AllBooks = () => {
   return (
     <div className="my-12 container mx-auto">
       <h2 className="font-bold text-3xl text-center mb-6">Books</h2>
-
+{/* add// */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         {books.map((book, ind) => {
           return <BookCard key={ind} book={book} />;
