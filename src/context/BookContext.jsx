b9232e4835b7153e6a5029316e3ea6b4e43418e5
ashik-billah <ashik.210101@s.pust.ac.bd>
2026-06-17 16:53:41 +0600

    store place

--- src/context/BookContext.jsx
+++ src/context/BookContext.jsx
@@ -19,6 +19,7 @@ const BookProvider = ({ children }) => {
     // step 1: store book id or store book object
     // addL
     // step 2: where to store
+    //add
     // step 2: array or collection
     //  step 3: If the book is already exist then show a alert or toast
     // step 4: if not then add the book in the array or collection
