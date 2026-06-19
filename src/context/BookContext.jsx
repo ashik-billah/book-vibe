8c1aaabe9092df98a53000f017f730f9d2f87186
ashik-billah <ashik.210101@s.pust.ac.bd>
2026-06-19 19:36:09 +0600

    book collection

--- src/context/BookContext.jsx
+++ src/context/BookContext.jsx
@@ -25,7 +25,7 @@ const BookProvider = ({ children }) => {
     //  step 3: If the book is already exist then show a alert or toast
     //add
     // step 4: if not then add the book in the array or collection
-
+//add
     addReadListToLocalDB(currentBook);
 
     const isExistBook = readList.find(
