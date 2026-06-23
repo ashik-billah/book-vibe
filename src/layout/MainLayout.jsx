593f52732551eb57715335739d2911965c506ef1
ashik-billah <ashik.210101@s.pust.ac.bd>
2026-06-23 20:14:09 +0600

    add main layout

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../components/shared/navbar/Navbar";
 import { Outlet } from "react-router";
-
+//add
 const MainLayout = () => {
   return (
     <div>
