536e23828141b4b63f653dfc24b99b0a9c4ec474
ashik-billah <ashik.210101@s.pust.ac.bd>
2026-06-27 09:27:11 +0600

    add error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router";
-
+//add
 const ErrorPage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 px-6">
